12a16f59eabdd23bb58c390700e2d7ae15f1a9bf
Sho Tamura <tam.nosuke15@gmail.com>
2024-11-29 00:34:58 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import restart from "vite-plugin-restart";
 export default {
   root: "src/", // Sources files (typically where index.html is)
   publicDir: "../static/", // Path from "root" to static assets (files that are served as they are)
+  base: "./",
   server: {
     host: true, // Open to local network and display URL
     open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
@@ -11,6 +12,13 @@ export default {
     outDir: "../docs", // Output in the dist/ folder
     emptyOutDir: true, // Empty the folder first
     sourcemap: true, // Add sourcemap
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          return "[name][extname]"; // アセットファイル名の形式を指定
+        },
+      },
+    },
   },
   plugins: [
     restart({ restart: ["../static/**"] }), // Restart server on static file change
